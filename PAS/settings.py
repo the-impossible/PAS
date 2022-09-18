5230e9a80a6ecbf5c9c419db1ad9eb9268f10d5e
the-impossible <richardemmanuel45@gmail.com>
2022-09-18 04:23:22 +0100

    Modified database settings

--- PAS/settings.py
+++ PAS/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'PAS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
